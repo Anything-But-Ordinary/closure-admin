378c3ea37b8188b39a213fe5b6f8699c52f8d479
Ruby <LIU10032@163.com>
2022-03-12 10:35:55 +0800

    feat: clear default file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return () => {
+      <>
+        <div>Hello Vue TSX</div>
+      </>;
+    };
+  },
+});
