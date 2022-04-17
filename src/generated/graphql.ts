1a25f86b3e9a22b137e3daf15a4f612428634456
Ruby <LIU10032@163.com>
2022-04-17 10:57:48 +0800

    feat: login form validate

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -16,39 +16,37 @@ export type Scalars = {
   Float: number;
 };
 
-export type CreateAccountInput = {
-  email: Scalars['String'];
-  password: Scalars['String'];
-  who: Who;
-};
-
-export type CreateAccountOutput = {
-  __typename?: 'CreateAccountOutput';
-  error?: Maybe<Scalars['String']>;
-  ok: Scalars['Boolean'];
-};
-
 export type LoginInput = {
   email: Scalars['String'];
   password: Scalars['String'];
 };
 
-export type LoginOutput = {
-  __typename?: 'LoginOutput';
-  error?: Maybe<Scalars['String']>;
-  ok: Scalars['Boolean'];
-  token?: Maybe<Scalars['String']>;
+export type LoginResponse = {
+  __typename?: 'LoginResponse';
+  isLogin: Scalars['Boolean'];
+  jwt?: Maybe<Scalars['String']>;
+};
+
+export type LogoutResponse = {
+  __typename?: 'LogoutResponse';
+  isLogout: Scalars['Boolean'];
+  message?: Maybe<Scalars['String']>;
+};
+
+export type MeQueryResponse = {
+  __typename?: 'MeQueryResponse';
+  email: Scalars['String'];
+  username: Scalars['String'];
 };
 
 export type Mutation = {
   __typename?: 'Mutation';
-  createAccount: CreateAccountOutput;
-  login: LoginOutput;
-};
-
-
-export type MutationCreateAccountArgs = {
-  input: CreateAccountInput;
+  login: LoginResponse;
+  logout: LogoutResponse;
+  register: RegisterResponse;
+  updatePassword: UpdatePasswordResponse;
+  updateUserInfo: UserInfoResponse;
+  user: WhoResponse;
 };
 
 
@@ -56,23 +54,67 @@ export type MutationLoginArgs = {
   input: LoginInput;
 };
 
+
+export type MutationRegisterArgs = {
+  input: RegisterInput;
+};
+
+
+export type MutationUpdatePasswordArgs = {
+  input: UpdatePasswordInput;
+};
+
+
+export type MutationUpdateUserInfoArgs = {
+  input: UserInfoInput;
+};
+
 export type Query = {
   __typename?: 'Query';
-  me: User;
-  sayHello: Scalars['String'];
+  foo: Scalars['String'];
+  me: MeQueryResponse;
 };
 
-export type User = {
-  __typename?: 'User';
+export type RegisterInput = {
   email: Scalars['String'];
   password: Scalars['String'];
-  who: Who;
+  password_confirm: Scalars['String'];
+  username: Scalars['String'];
 };
 
-export enum Who {
-  Owner = 'Owner',
-  Visitor = 'Visitor'
-}
+export type RegisterResponse = {
+  __typename?: 'RegisterResponse';
+  isRegister: Scalars['Boolean'];
+};
+
+export type UpdatePasswordInput = {
+  password: Scalars['String'];
+  password_confirm: Scalars['String'];
+};
+
+export type UpdatePasswordResponse = {
+  __typename?: 'UpdatePasswordResponse';
+  message: Scalars['String'];
+  updated: Scalars['Boolean'];
+};
+
+export type UserInfoInput = {
+  email?: InputMaybe<Scalars['String']>;
+  username?: InputMaybe<Scalars['String']>;
+};
+
+export type UserInfoResponse = {
+  __typename?: 'UserInfoResponse';
+  message?: Maybe<Scalars['String']>;
+  updated: Scalars['Boolean'];
+};
+
+export type WhoResponse = {
+  __typename?: 'WhoResponse';
+  isAuthed: Scalars['Boolean'];
+  message?: Maybe<Scalars['String']>;
+  username?: Maybe<Scalars['String']>;
+};
 
 export type LoginMutationVariables = Exact<{
   email: Scalars['String'];
@@ -80,19 +122,29 @@ export type LoginMutationVariables = Exact<{
 }>;
 
 
-export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'LoginOutput', ok: boolean, error?: string | null } };
+export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'LoginResponse', isLogin: boolean, jwt?: string | null } };
+
+export type RegisterMutationVariables = Exact<{
+  password: Scalars['String'];
+  password_confirm: Scalars['String'];
+  email: Scalars['String'];
+  username: Scalars['String'];
+}>;
+
+
+export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'RegisterResponse', isRegister: boolean } };
 
 export type Unnamed_1_QueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type Unnamed_1_Query = { __typename?: 'Query', me: { __typename?: 'User', email: string } };
+export type Unnamed_1_Query = { __typename?: 'Query', me: { __typename?: 'MeQueryResponse', email: string, username: string } };
 
 
 export const LoginDocument = gql`
     mutation Login($email: String!, $password: String!) {
   login(input: {email: $email, password: $password}) {
-    ok
-    error
+    isLogin
+    jwt
   }
 }
     `;
@@ -119,10 +171,45 @@ export function useLoginMutation(options: VueApolloComposable.UseMutationOptions
   return VueApolloComposable.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
 }
 export type LoginMutationCompositionFunctionResult = VueApolloComposable.UseMutationReturn<LoginMutation, LoginMutationVariables>;
+export const RegisterDocument = gql`
+    mutation Register($password: String!, $password_confirm: String!, $email: String!, $username: String!) {
+  register(
+    input: {password: $password, password_confirm: $password_confirm, email: $email, username: $username}
+  ) {
+    isRegister
+  }
+}
+    `;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a Vue component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns an object that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - Several other properties: https://v4.apollo.vuejs.org/api/use-mutation.html#return
+ *
+ * @param options that will be passed into the mutation, supported options are listed on: https://v4.apollo.vuejs.org/guide-composable/mutation.html#options;
+ *
+ * @example
+ * const { mutate, loading, error, onDone } = useRegisterMutation({
+ *   variables: {
+ *     password: // value for 'password'
+ *     password_confirm: // value for 'password_confirm'
+ *     email: // value for 'email'
+ *     username: // value for 'username'
+ *   },
+ * });
+ */
+export function useRegisterMutation(options: VueApolloComposable.UseMutationOptions<RegisterMutation, RegisterMutationVariables> | ReactiveFunction<VueApolloComposable.UseMutationOptions<RegisterMutation, RegisterMutationVariables>>) {
+  return VueApolloComposable.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
+}
+export type RegisterMutationCompositionFunctionResult = VueApolloComposable.UseMutationReturn<RegisterMutation, RegisterMutationVariables>;
 export const Document = gql`
     {
   me {
     email
+    username
   }
 }
     `;
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -8,9 +8,13 @@ import {
   NRow,
   NSpace,
   NGradientText,
+  useMessage,
+  type FormItemInst,
+  type FormItemRule,
+  type FormRules,
   type FormInst,
 } from "naive-ui";
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, type Ref } from "vue";
 import { LoginHeader } from "../../layouts/header";
 import { useRouter } from "vue-router";
 
@@ -19,6 +23,49 @@ export default defineComponent({
     const router = useRouter();
 
     const formRef = ref<FormInst | null>(null);
+    const message = useMessage();
+
+    interface ModelType {
+      email: Ref<string | null>;
+      password: string | null;
+    }
+
+    const model = ref<ModelType>({
+      email: ref(null),
+      password: null,
+    });
+
+    const rules: FormRules = {
+      email: [
+        {
+          required: true,
+          message: "需要邮箱",
+          trigger: ["blur"],
+        },
+        {
+          validator(rule: FormItemRule, value: string) {
+            // console.log("value", value);
+            console.log("where:", value?.indexOf("@"));
+            if (value === (null || "")) {
+              return true;
+            }
+            return (
+              (value as string)?.indexOf("@") !== -1 &&
+              (value as string)?.indexOf(".") !== -1
+            );
+          },
+          message: "邮箱不合法",
+          trigger: ["input", "blur"],
+        },
+      ],
+      password: [
+        {
+          required: true,
+          message: "请输入密码",
+          trigger: ["blur", "input"],
+        },
+      ],
+    };
 
     return () => (
       <>
@@ -29,12 +76,34 @@ export default defineComponent({
               title={"Do you want to write a Letter?"}
               class="mt-14 text-center bg-light-800  h-3/4 font-mono rounded-md md:w-4/5 md:h-4/5 lg:w-auto lg:h-auto shadow-lg"
             >
-              <NForm ref={formRef}>
-                <NFormItem label="email">
-                  <NInput />
+              <NForm ref={formRef} model={model.value} rules={rules}>
+                <NFormItem label="email" path="email">
+                  <NInput
+                    onChange={(e) => {
+                      // console.log("onChange:", e);
+                      model.value.email = e;
+                    }}
+                    onInput={(e) => {
+                      // console.log(e);
+                      model.value.email = e;
+                      console.log("email", model.value.email);
+                    }}
+                    value={model.value.email}
+                    // ref={model.value.email}
+                  />
                 </NFormItem>
-                <NFormItem label="password">
-                  <NInput />
+                <NFormItem label="password" path="password">
+                  <NInput
+                    type="password"
+                    value={model.value.password}
+                    onInput={(e) => {
+                      model.value.password = e;
+                      console.log("password", model.value.password);
+                    }}
+                    onChange={(e) => {
+                      model.value.password = e;
+                    }}
+                  />
                 </NFormItem>
                 <NSpace justify="center">
                   <NGradientText type="info">
